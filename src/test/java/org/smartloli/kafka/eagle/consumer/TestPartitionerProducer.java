f4e181b088f48db736001c91a98293586be2ce76
smartloli <smartloli.org@gmail.com>
2017-02-09 09:59:39 +0800

    add test simple consumer interface

--- src/test/java/org/smartloli/kafka/eagle/consumer/TestPartitionerProducer.java
+++ src/test/java/org/smartloli/kafka/eagle/consumer/TestPartitionerProducer.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.smartloli.kafka.eagle.consumer;
 
 import java.util.Properties;
@@ -10,11 +27,11 @@ import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
 
 /**
- * @Date Mar 9, 2016
+ * Test kafka8 topic partition data producer.
  *
- * @Author dengjie
+ * @author smartloli.
  *
- * @Note TODO
+ *         Created by Mar 14, 2016
  */
 public class TestPartitionerProducer {
 
--- src/test/java/org/smartloli/kafka/eagle/consumer/TestSimpleKafkaConsumer.java
+++ src/test/java/org/smartloli/kafka/eagle/consumer/TestSimpleKafkaConsumer.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.smartloli.kafka.eagle.consumer;
 
 import java.nio.ByteBuffer;
@@ -25,11 +42,11 @@ import kafka.javaapi.consumer.SimpleConsumer;
 import kafka.message.MessageAndOffset;
 
 /**
- * @Date Mar 2, 2016
+ * Kafka official low consumer sample.
  *
- * @Author dengjie
+ * @author smartloli.
  *
- * @Note Simple consumer api
+ *         Created by Mar 14, 2016
  */
 public class TestSimpleKafkaConsumer extends Thread {
 	private static Logger log = LoggerFactory.getLogger(TestSimpleKafkaConsumer.class);
