70d9d4a5591ea4277af16d31d55c279e42450248
smartloli <smartloli.org@gmail.com>
2019-07-27 02:24:04 +0800

    modify ke.sh

--- kafka-eagle-web/src/main/resources/bash/ke.sh
+++ kafka-eagle-web/src/main/resources/bash/ke.sh
@@ -22,30 +22,6 @@
 # Author smartloli.
 # Update by Jul 27, 2019
 
-#!/bin/bash
-
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Used to manage Kafka Eagle system start, stop, restart and other operations
-#
-#    http://www.kafka-eagle.org/
-#
-# Author smartloli.
-# Update by Jul 27, 2019
-
 export MALLOC_ARENA_MAX=1
 
 stime=`date "+%Y-%m-%d %H:%M:%S"`
