87681406456a8c939b115226882f532caf326e66
zishuangzhu <xinghaixuanwo@outlook.com>
2019-12-16 11:56:18 +0800

    解决windows下启动时找不到路径的问题
    在所有使用环境变量的地方加上双引号

--- kafka-eagle-web/src/main/resources/bash/ke.bat
+++ kafka-eagle-web/src/main/resources/bash/ke.bat
@@ -21,44 +21,44 @@ goto end
 
 :okHome
 
-if exist %KE_HOME%\kms\webapps\ke (
+if exist "%KE_HOME%\kms\webapps\ke" (
     rem Delete ke dir menu.
-    rd /s /q %KE_HOME%\kms\webapps\ke
+    rd /s /q "%KE_HOME%\kms\webapps\ke"
 )
 
-if exist %KE_HOME%\kms\work (
+if exist "%KE_HOME%\kms\work" (
     rem Delete tomcat work dir menu.
-    rd /s /q %KE_HOME%\kms\work
+    rd /s /q "%KE_HOME%\kms\work"
 )
 
-md %KE_HOME%\kms\webapps\ke
-cd %KE_HOME%\kms\webapps\ke
+md "%KE_HOME%\kms\webapps\ke"
+cd "%KE_HOME%\kms\webapps\ke"
 rem echo Current Path :: %cd%
-%JAVA_HOME%\bin\jar -xvf %KE_HOME%\kms\webapps\ke.war
+"%JAVA_HOME%\bin\jar" -xvf "%KE_HOME%\kms\webapps\ke.war"
 
 ping /n 2 127.1 >nul
 
-cd %KE_HOME%
+cd "%KE_HOME%"
 SET CLSNAME=org.smartloli.kafka.eagle.plugin.server.TomcatServerListen
 
 setlocal enabledelayedexpansion
-for %%i in (%KE_HOME%\kms\webapps\ke\WEB-INF\lib\dom*.jar,%KE_HOME%\kms\webapps\ke\WEB-INF\lib\log4j*.jar,%KE_HOME%\kms\webapps\ke\WEB-INF\lib\slf4j*.jar,%KE_HOME%\kms\webapps\ke\WEB-INF\lib\kafka-eagle*.jar) do (
+for %%i in ("%KE_HOME%\kms\webapps\ke\WEB-INF\lib\dom*.jar","%KE_HOME%\kms\webapps\ke\WEB-INF\lib\log4j*.jar","%KE_HOME%\kms\webapps\ke\WEB-INF\lib\slf4j*.jar","%KE_HOME%\kms\webapps\ke\WEB-INF\lib\kafka-eagle*.jar") do (
   set CLSPATH=!CLSPATH!;%%i
 )
 rem echo %CLSPATH%
-%JAVA_HOME%\bin\java -classpath %CLSPATH% %CLSNAME% >> %KE_HOME%\logs\ke.out
+"%JAVA_HOME%\bin\java" -classpath %CLSPATH% %CLSNAME% >> "%KE_HOME%\logs\ke.out"
 
 echo Kafka Eagle system monitor port successful...
 
 ping /n 3 127.1 >nul
 
-if exist %KE_HOME%\kms\webapps\ke\WEB-INF\classes\*.properties (
-  rd /s /q %KE_HOME%\kms\webapps\ke\WEB-INF\classes\*.properties
+if exist "%KE_HOME%\kms\webapps\ke\WEB-INF\classes\*.properties" (
+  rd /s /q "%KE_HOME%\kms\webapps\ke\WEB-INF\classes\*.properties"
 )
-copy %KE_HOME%\conf\*.properties %KE_HOME%\kms\webapps\ke\WEB-INF\classes\
+copy "%KE_HOME%\conf\*.properties" "%KE_HOME%\kms\webapps\ke\WEB-INF\classes\"
 
 ping /n 3 127.1 >nul
-set CATALINA_HOME=%KE_HOME%\kms
-call  %KE_HOME%\kms\bin\startup.bat
+set CATALINA_HOME="%KE_HOME%\kms"
+call  "%KE_HOME%\kms\bin\startup.bat"
 
 :end
