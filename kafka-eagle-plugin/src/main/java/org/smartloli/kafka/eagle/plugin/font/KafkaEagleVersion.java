5eb024c204341acdee8f6d89ca3ae76f962a5a83
smartloli <smartloli.org@gmail.com>
2020-08-11 17:36:02 +0800

    modify pom

--- kafka-eagle-plugin/src/main/java/org/smartloli/kafka/eagle/plugin/font/KafkaEagleVersion.java
+++ kafka-eagle-plugin/src/main/java/org/smartloli/kafka/eagle/plugin/font/KafkaEagleVersion.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,20 +22,20 @@ import java.io.IOException;
 
 /**
  * Print kafka eagle system version.
- * 
+ *
  * @author smartloli.
  *
  *         Created by Jan 23, 2018
  */
 public class KafkaEagleVersion {
 
-	public static void main(String[] args) throws IOException {
-		String name = System.getProperty("user.dir") + "/font/slant.flf";
-		File file = new File(name);
-		String asciiArt = FigletFont.convertOneLine(file, "KAfKA EAGLE");
-		System.out.println("Welcome to");
-		System.out.println(asciiArt);
-		System.out.println("Version 2.0.1 -- Copyright 2016-2020");
-	}
+    public static void main(String[] args) throws IOException {
+        String name = System.getProperty("user.dir") + "/font/slant.flf";
+        File file = new File(name);
+        String asciiArt = FigletFont.convertOneLine(file, "KAfKA EAGLE");
+        System.out.println("Welcome to");
+        System.out.println(asciiArt);
+        System.out.println("Version 2.0.2 -- Copyright 2016-2020");
+    }
 
 }
