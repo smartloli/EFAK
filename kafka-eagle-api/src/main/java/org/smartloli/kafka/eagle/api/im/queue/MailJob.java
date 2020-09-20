595331f21f2fd707eef24f9a96e9b8093b3f3a76
smartloli <smartloli.org@gmail.com>
2020-09-20 20:12:45 +0800

    modify log

--- kafka-eagle-api/src/main/java/org/smartloli/kafka/eagle/api/im/queue/MailJob.java
+++ kafka-eagle-api/src/main/java/org/smartloli/kafka/eagle/api/im/queue/MailJob.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,51 +17,47 @@
  */
 package org.smartloli.kafka.eagle.api.im.queue;
 
-import java.util.Arrays;
-
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.http.message.BasicNameValuePair;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.smartloli.kafka.eagle.common.protocol.alarm.queue.BaseJobContext;
+import org.smartloli.kafka.eagle.common.util.ErrorUtils;
 import org.smartloli.kafka.eagle.common.util.HttpClientUtils;
 import org.smartloli.kafka.eagle.common.util.KConstants.AlarmQueue;
-import org.smartloli.kafka.eagle.common.util.ErrorUtils;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import java.util.Arrays;
 
 /**
  * Add alarm message to wechat job queue.
- * 
- * @author smartloli.
  *
- *         Created by Oct 27, 2019
+ * @author smartloli.
+ * <p>
+ * Created by Oct 27, 2019
  */
 public class MailJob implements Job {
 
-	private Logger LOG = LoggerFactory.getLogger(MailJob.class);
+    /**
+     * Send alarm information by mail or webhook.
+     */
+    public void execute(JobExecutionContext jobContext) throws JobExecutionException {
+        BaseJobContext bjc = (BaseJobContext) jobContext.getJobDetail().getJobDataMap().get(AlarmQueue.JOB_PARAMS);
+        sendMsg(bjc.getData(), bjc.getUrl());
+    }
 
-	/** Send alarm information by mail or webhook. */
-	public void execute(JobExecutionContext jobContext) throws JobExecutionException {
-		BaseJobContext bjc = (BaseJobContext) jobContext.getJobDetail().getJobDataMap().get(AlarmQueue.JOB_PARAMS);
-		sendMsg(bjc.getData(), bjc.getUrl());
-	}
-
-	private int sendMsg(String data, String url) {
-		try {
-			JSONObject object = JSON.parseObject(data);
-			BasicNameValuePair address = new BasicNameValuePair("address", object.getString("address"));
-			BasicNameValuePair msg = new BasicNameValuePair("msg", object.getString("msg"));
-			HttpClientUtils.doPostForm(url, Arrays.asList(address, msg));
-		} catch (Exception e) {
-			ErrorUtils.print(this.getClass()).error("Send alarm message has error by mail, msg is ", e);
-			LOG.error("Send alarm message has error by mail, msg is ", e);
-			return 0;
-		}
-		return 1;
-	}
+    private int sendMsg(String data, String url) {
+        try {
+            JSONObject object = JSON.parseObject(data);
+            BasicNameValuePair address = new BasicNameValuePair("address", object.getString("address"));
+            BasicNameValuePair msg = new BasicNameValuePair("msg", object.getString("msg"));
+            HttpClientUtils.doPostForm(url, Arrays.asList(address, msg));
+        } catch (Exception e) {
+            ErrorUtils.print(this.getClass()).error("Send alarm message has error by mail, msg is ", e);
+            return 0;
+        }
+        return 1;
+    }
 
 }
