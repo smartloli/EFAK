e696a64bd0a8c035ebf9edaa0b85b67eab556052
Max.Tian <max.tian@sap.com>
2021-05-07 09:49:23 +0800

    Revert "Added some git ignores and modified pom files for updating newer version libraries."
    This reverts commit 05f8c5bd484218c1665a19e650983cd31ae3dd69.

--- kafka-eagle-web/src/main/java/org/smartloli/kafka/eagle/web/controller/TopicController.java
+++ kafka-eagle-web/src/main/java/org/smartloli/kafka/eagle/web/controller/TopicController.java
@@ -21,7 +21,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.base.Strings;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.kafka.clients.admin.ConfigEntry;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
