d3dc5a3350d03344035bf3650acbf453c528db07
smartloli <smartloli.org@gmail.com>
2022-03-14 00:57:51 +0800

    add comments and author

--- efak-web/src/main/java/org/smartloli/kafka/eagle/web/quartz/shard/task/alert/ClusterStrategy.java
+++ efak-web/src/main/java/org/smartloli/kafka/eagle/web/quartz/shard/task/alert/ClusterStrategy.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.smartloli.kafka.eagle.web.quartz.shard.task.alert;
 
 import org.smartloli.kafka.eagle.common.protocol.alarm.AlarmClusterInfo;
@@ -5,6 +22,13 @@ import org.smartloli.kafka.eagle.common.protocol.alarm.AlarmConfigInfo;
 import org.smartloli.kafka.eagle.core.metrics.KafkaMetricsService;
 import org.smartloli.kafka.eagle.web.service.impl.AlertServiceImpl;
 
+/**
+ * Unified interface entrance of alarm module.
+ *
+ * @author LambertCOL.
+ * <p>
+ * Created by Dec 26, 2021
+ */
 public interface ClusterStrategy {
 
     void execute(AlarmClusterInfo cluster, AlarmConfigInfo alarmConfig, KafkaMetricsService kafkaMetricsService, AlertServiceImpl alertService);
--- efak-web/src/main/java/org/smartloli/kafka/eagle/web/quartz/shard/task/alert/TopicClusterStrategy.java
+++ efak-web/src/main/java/org/smartloli/kafka/eagle/web/quartz/shard/task/alert/TopicClusterStrategy.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.smartloli.kafka.eagle.web.quartz.shard.task.alert;
 
 import com.alibaba.fastjson.JSON;
@@ -13,6 +30,13 @@ import org.smartloli.kafka.eagle.web.service.impl.TopicServiceImpl;
 import java.util.*;
 import java.util.regex.Pattern;
 
+/**
+ * Topic alarm information.
+ *
+ * @author LambertCOL.
+ * <p>
+ * Created by Dec 26, 2021
+ */
 public class TopicClusterStrategy implements ClusterStrategy {
 
     @Override
