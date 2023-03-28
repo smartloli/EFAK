28c6ad4e223701a07a12336022d0cdd75136a894
linhf <linhf@rimepevc.com>
2023-03-28 16:49:18 +0800

    [UPD] remove password col in user management page

--- efak-web/src/main/webapp/media/js/main/system/user.js
+++ efak-web/src/main/webapp/media/js/main/system/user.js
@@ -14,9 +14,7 @@ $(document).ready(function () {
             "mData": 'realname'
         }, {
             "mData": 'email'
-        }, {
-            "mData": 'password'
-        }, {
+        },{
             "mData": 'operate'
         }]
     });
