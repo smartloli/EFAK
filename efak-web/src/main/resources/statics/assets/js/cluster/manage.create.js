0ad51155c3678521617a89c03fe74c884e46f9b8
smartloli <smartloli.org@gmail.com>
2023-05-28 23:07:29 +0800

    add: create cluster view

--- efak-web/src/main/resources/statics/assets/js/cluster/manage.create.js
+++ efak-web/src/main/resources/statics/assets/js/cluster/manage.create.js
@@ -1,4 +1,26 @@
 // go clusters manage create
+// batch import
+try {
+    var dr = $("#efak_kafka_broker_batch_import").dropify({
+        messages: {
+            'default': '将文件拖放到此处或单击',
+            'replace': '拖放或单击文件来替换',
+            'remove': '删除文件',
+            'error': '抱歉，上传出现异常'
+        },
+        error: {
+            'fileSize': '上传文件大小超过最大值 ({{ value }})',
+            'fileExtension': '文件类型不正确，仅支持 {{ value }} 文件类型',
+        }
+    });
+    dr.on('change', function () {
+        var file = this.files[0];
+        console.log(file);
+    });
+} catch (e) {
+    console.log(e)
+}
+
 $("#efka_kafka_kraft_enable").click(function () {
     $("#efak_kraft_enable_checked").show();
     $("#efak_kraft_disable_checked").hide();
@@ -16,3 +38,13 @@ $("#efka_kafka_kraft_disable").click(function () {
 $("#efak_cluster_create_cancle").click(function () {
     window.location.href = '/clusters/manage';
 });
+
+// import is null alert
+function importFilesIsNull(){
+    var file = $("#efak_kafka_broker_batch_import")[0];
+    if (file.value == "") {
+        alert("请选择文件");
+        return false;
+    }
+    return true;
+}
