693b7f3cefeff911f0030a8d36b853f9f35d5787
smartloli <smartloli.org@gmail.com>
2023-06-17 23:45:01 +0800

    add: create topic retain

--- efak-web/src/main/resources/statics/assets/js/topic/topic.create.js
+++ efak-web/src/main/resources/statics/assets/js/topic/topic.create.js
@@ -7,3 +7,8 @@ $("#efak_topic_name_create").maxlength({
     warningClass: "badge mt-1 bg-success",
     limitReachedClass: "badge mt-1 bg-danger"
 });
+
+if ($("#efak_topic_name_retain_unit_create").length) {
+    console.log("select2");
+    $("#efak_topic_name_retain_unit_create").select2();
+}
